e813566210c31f82d2efac7caa1e25e776fb962d
SUJIN J <sjnnus@gmail.com>
2026-06-09 09:58:08 +0900

    DB 타임존 오류 수정(UTC -> KST)

--- src/main/java/com/strnd/api/StrndApplication.java
+++ src/main/java/com/strnd/api/StrndApplication.java
@@ -3,10 +3,14 @@ package com.strnd.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class StrndApplication {
 
 	public static void main(String[] args) {
+		// JVM 기본 타임존을 KST로 고정
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(StrndApplication.class, args);
 	}
 
